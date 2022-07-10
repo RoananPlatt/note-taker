977c27134791f5913cf579be95f13473d241550c
Roanan Platt <roanan.platt@gmail.com>
2022-07-10 13:06:15 -0600

    server

--- server.js
+++ server.js
@@ -10,31 +10,16 @@ const html = require("./routes/html");
 const app = express();
 var PORT = process.env.PORT || 3001
 
-
-// Middleware
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(express.static('public'));
 
 app.use("/api", api);
-// app.use("/", html);
 
-// call for notes.html
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-// POSt function for adding new notes to db.json 
-// app.post("/api/notes", function (req, res){
-//   console.log('Here I am');
-//   const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-//   newNotes.id = uuid.v4();
-//   notes.push(newNotes);
-//   fs.writeFileSync("./db/db.json", JSON.stringify(notes));
-//   res.json(notes);
-// });
-
-// Start listen
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
