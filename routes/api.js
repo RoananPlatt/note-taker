9287b3e736f4b95e72152c2a74fa14bc3c4c1430
Roanan Platt <roanan.platt@gmail.com>
2022-07-10 13:02:30 -0600

    routes

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,7 @@ const store = require("../db/store");
 
 router.get("/notes", (req, res) => {
   console.log('Here I am');
+
   store
     .getNotes()
     .then((notes) => res.json(notes))
@@ -12,6 +13,7 @@ router.get("/notes", (req, res) => {
 
 
 router.post("/notes", (req, res) => {
+
   store
     .addNote(req.body)
     .then((note) => res.json(note))
@@ -20,9 +22,12 @@ router.post("/notes", (req, res) => {
 
 
 router.delete("/notes/:id", (req, res) => {
+
   store
     .removeNote(req.params.id)
     .then(() => res.json({ ok: true }))
     .catch((err) => res.status(500).json(err));
 });
+
+
 module.exports = router;
